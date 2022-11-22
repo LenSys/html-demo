0acbf6c6942dbdac209fb87b24afa03c35515df0
LenSys <[omitted]>
2022-11-22 14:25:22 +0100

    added click button with opacity to close dropdown menu

--- Frontend-Practice/vue-tailwind-build-dropdown-menu/src/components/AccountDropdown.vue
+++ Frontend-Practice/vue-tailwind-build-dropdown-menu/src/components/AccountDropdown.vue
@@ -3,6 +3,7 @@
     <button @click="isOpen = !isOpen" class="block h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
       <img class="h-full w-full object-cover" src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80" alt="Your avatar" />
     </button>
+    <button v-if="isOpen" @click="isOpen = false" class="fixed h-full w-full top-0 left-0 bottom-0 right-0 bg-black opacity-50"></button>
     <div v-if="isOpen" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
       <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Account settings</a>
       <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Support</a>
