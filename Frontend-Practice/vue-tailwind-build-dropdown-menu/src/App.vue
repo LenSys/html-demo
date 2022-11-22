d3af54e714ba60583464dd6cd1ccc37278c4029b
LenSys <[omitted]>
2022-11-22 11:30:06 +0100

    modified App.vue

--- Frontend-Practice/vue-tailwind-build-dropdown-menu/src/App.vue
+++ Frontend-Practice/vue-tailwind-build-dropdown-menu/src/App.vue
@@ -1,9 +1,19 @@
-<script setup>
-import Navbar from './components/Navbar.vue'
-</script>
-
 <template>
-  <Navbar />
+  <div id="app" class="antialiased text-gray-900">
+    <div class="bg-gray-800 min-h-screen py-32 px-6">
+      <AccountDropdown/>
+    </div>
+  </div>
 </template>
+
+<script>
+import AccountDropdown from './components/AccountDropdown'
+export default {
+  name: 'app',
+  components: {
+    AccountDropdown
+  },
+}
+</script>
 
 <style src="./assets/tailwind.css"></style>
