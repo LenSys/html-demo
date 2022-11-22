1822d96201bfbe598d244aa670c291a7a6b8a247
LenSys <[omitted]>
2022-11-22 11:31:20 +0100

    fixed display bug

--- Frontend-Practice/vue-tailwind-build-dropdown-menu/src/App.vue
+++ Frontend-Practice/vue-tailwind-build-dropdown-menu/src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import AccountDropdown from './components/AccountDropdown'
+import AccountDropdown from './components/AccountDropdown.vue'
 export default {
   name: 'app',
   components: {
