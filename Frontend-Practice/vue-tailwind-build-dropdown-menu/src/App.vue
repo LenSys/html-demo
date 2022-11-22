14b2a675a2a755a6ee7bc179618f227cad211019
LenSys <[omitted]>
2022-11-22 12:22:32 +0100

    added account dropdown to navbar

--- Frontend-Practice/vue-tailwind-build-dropdown-menu/src/App.vue
+++ Frontend-Practice/vue-tailwind-build-dropdown-menu/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app" class="antialiased text-gray-900">
-    <div class="bg-gray-800 min-h-screen py-32 px-6">
-      <AccountDropdown />
+    <div class="bg-gray-200 min-h-screen">
+      <Navbar/>
     </div>
   </div>
 </template>
 
 <script setup>
-import AccountDropdown from './components/AccountDropdown.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <style src="./assets/tailwind.css"></style>
