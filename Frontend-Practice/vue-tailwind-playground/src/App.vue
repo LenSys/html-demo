8d16e0a04328738f7164c41fcc0ba34789ba3f2d
LenSys <[omitted]>
2022-11-24 09:38:03 +0100

    added modal dialogs components

--- Frontend-Practice/vue-tailwind-playground/src/App.vue
+++ Frontend-Practice/vue-tailwind-playground/src/App.vue
@@ -3,6 +3,25 @@
     <div class="bg-gray-200 min-h-screen">
       <Navbar/>
       <FormHelper />
+      <ModalDialog v-if="showModal" theme="sale" @closeModal="toggleModal">
+        <h3 class="font-bold text-4xl">Sale 50%</h3>
+        <p>We offer 50% off the current price for sale!</p>
+        <template v-slot:links>
+          <a href="#" class="px-4 py-2 bg-green-600 text-white">Buy now</a>
+          <a href="#" class="ml-2 px-4 py-2 bg-gray-500 text-white">More info</a>
+        </template>
+      </ModalDialog>
+      <ModalDialog v-if="showModalTwo" @closeModal="toggleModalTwo">
+        <h3 class="font-bold text-4xl">Sign up for the newsletter</h3>
+        <p>Get updates and promo codes!</p>
+        <template v-slot:links>
+          <a href="#" class="px-4 py-2 bg-green-600 text-white">Sign up now</a>
+        </template>
+      </ModalDialog>
+      <div class="container text-end">
+        <button @click="toggleModal" class="px-8 py-4 bg-indigo-600 text-white">Sale 50% Dialog</button>
+        <button @click="toggleModalTwo" class="ml-4 px-8 py-4 bg-indigo-600 text-white">Newsletter Dialog</button>
+      </div>
     </div>
   </div>
 </template>
@@ -10,6 +29,32 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
 import FormHelper from './components/FormHelper.vue'
+import ModalDialog from './components/ModalDialog.vue'
+</script>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    Navbar, 
+    FormHelper,
+    ModalDialog
+  },
+  data() {
+    return {
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo 
+    }
+  }
+}
 </script>
 
 <style src="./assets/tailwind.css"></style>
---
+++ Frontend-Practice/vue-tailwind-playground/src/components/ModalDialog.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="absolute backdrop inset-0" @click.self="closeModel">
+        <div class="modal-dialog relative mx-auto w-1/2 bg-white p-10" :class="[theme]">
+            <slot></slot>
+            <div class="mt-10 text-center">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.backdrop {
+    background-color: rgba(0,0,0,0.5);
+}
+
+.modal-dialog {
+    margin-top: 25%;
+}
+
+.modal-dialog.sale {
+    background-color: #101726;
+}
+
+.modal-dialog.sale h3 {
+    color: white;
+}
+
+.modal-dialog.sale p {
+    color: white;
+}
+</style>
+
+<script>
+export default {
+  name: "ModalDialog",
+  props: [
+    'theme'
+  ],
+  methods: {
+    closeModel() {
+        this.$emit('closeModal');
+    }
+  }
+}
+</script>
