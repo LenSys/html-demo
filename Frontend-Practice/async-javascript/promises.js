0259af3bfbc6209668b8970a5b759c72a9fd128b
LenSys <[omitted]>
2022-11-28 12:44:54 +0100

    added promises and then() to simulate async behaviour

--- Frontend-Practice/async-javascript/promises.js
+++ Frontend-Practice/async-javascript/promises.js
@@ -20,3 +20,34 @@ async function fetchUsers() {
 }
 
 fetchUsers();
+
+
+function loginUser(email, password) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Now we have the data");
+            resolve({ userEmail: email });
+        }, 3000);
+    });
+}
+
+function getUserVideos(email) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['video1', 'video2', 'video3']);
+        }, 2000);
+    });
+}
+
+function videoDetails(video) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("title of the video");
+        }, 2000);
+    });
+}
+
+loginUser('demouser@gmail.com', "123456")
+    .then(user => getUserVideos(user.email))
+    .then(videos => videoDetails(videos[0]))
+    .then(detail => console.log(detail));
