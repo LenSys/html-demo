ade9f953a0ee81bd6f3a56ce5c57cd07c3f47504
LenSys <[omitted]>
2022-11-28 08:52:44 +0100

    created todo app in vue.js

---
+++ Frontend-Practice/vue-todo-app/README.md
@@ -0,0 +1,28 @@
+# vue-todo-app
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Vue.js Todo App Screenshot
+![Vue.js Todo App](vue-todo-app.gif "Vue.js Todo App")
---
+++ Frontend-Practice/vue-todo-app/src/App.vue
@@ -0,0 +1,110 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue';
+
+const todos = ref([])
+const name = ref('')
+
+const input_content = ref('')
+const input_category = ref(null);
+
+// sort todos
+const todos_asc = computed(() => todos.value.sort((a, b) => {
+  return b.createdAt - a.createdAt
+}));
+
+// add todo item to todos array on submit
+const addTodo = () => {
+  if(input_content.value.trim() === '' || input_category === null) {
+    return;
+  }
+
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime()
+  });
+
+  // reset input data
+  input_content.value = '';
+  input_category.value = null;
+}
+
+// remove todo item from todos array on delete
+const removeTodo = todo => {
+  todos.value = todos.value.filter(t => t !== todo);
+}
+
+/* 
+watch for changes in todo arrays 
+(deep: watch for elements being added or removed from todos array,
+ otherwise it only watches for complete change of todos assignment!)
+*/
+watch(todos, newVal => {
+  localStorage.setItem('todos', JSON.stringify(newVal))
+}, { deep: true })
+
+/*
+watch for changes in name
+*/
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || '';
+  todos.value = JSON.parse(localStorage.getItem('todos')) || [];
+})
+
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name">
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input type="text" placeholder="e.g. make a video" v-model="input_content">
+
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input type="radio" name="category" id="category1" value="business" v-model="input_category">
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+          <label>
+            <input type="radio" name="category" id="category2" value="personal" v-model="input_category">
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+        <input type="submit" value="Add todo">
+      </form>
+    </section>
+    
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${ todo.category }`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
